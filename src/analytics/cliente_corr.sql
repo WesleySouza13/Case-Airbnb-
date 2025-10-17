7426748b9993c69c7958fd402af8ac17f0ae5270
WesleySouza13 <souzabusiness13@gmail.com>
2025-10-17 17:38:51 -0300

    criaçao das tabelas: tempoHost e cliente_corr. Analise de correlaçao entre tempo de host x preço x rating

---
+++ src/analytics/cliente_corr.sql
@@ -0,0 +1,13 @@
+-- Quais fatores mais influenciam uma boa avaliação?
+-- irei selecionar algumas variaveis para fazer um estudo de correlaçao (nao causa) 
+select 
+    room_type, 
+    beds,
+    bed_type,
+    bathrooms,
+    bedrooms,
+    accommodates,
+    review_scores_rating, 
+    cleaning_fee as taxa_de_limpeza,
+    host_is_superhost
+from listings
