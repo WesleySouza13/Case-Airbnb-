2d6a1a89b08ba3180c57afe668d6080ccff3170e
WesleySouza13 <souzabusiness13@gmail.com>
2025-10-17 16:42:08 -0300

    estudo tempo de host x avaliaçao e preço

---
+++ src/analytics/tempoHost.sql
@@ -0,0 +1,21 @@
+--- irei investigar antigos têm melhor desempenho e se merecem incentivos
+
+with tb_tempoHost as (select 
+    id,
+    host_since, 
+    (last_scraped - host_since) as tempoHost,
+    review_scores_rating,
+    cast(REPLACE(price, '$', '') as real) as price$, 
+    (last_review - first_review) as diferencaUltimoReview
+from listings
+where host_since is not null -- para nao retornar valores de tempo nulos
+)
+
+SELECT 
+*, 
+CASE
+    when tempoHost = 1 THEN 'host_novo'
+    WHEN tempoHost > 1 AND tempoHost <= 3 THEN 'host_medio'
+    else 'host_antigo'
+end as binTempo
+from tb_tempoHost;
