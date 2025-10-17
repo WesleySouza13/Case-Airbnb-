a4867df2f08946c2ea484d9afe38266ebdef136a
WesleySouza13 <souzabusiness13@gmail.com>
2025-10-17 15:43:39 -0300

    estudo de serie: freq_preço_medio

--- src/analytics/freq_series.sql
+++ src/analytics/freq_series.sql
@@ -1,3 +1,5 @@
+--- estou calculando a frequencia de alugueis disponiveis em relaçao aos dias
+--- tambem estou buscando entender o preço médio dos alugueis em relaçao ao tempo, para ver se sofre de relaçao sazonal ou se há curva de tendencia 
 select 
     date, 
     count(available) as frequencia,
